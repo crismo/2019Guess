f6d5637a502561ed1438b72bb5495c411d763510
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-10-07 16:47:51 +0200

    language fix

--- language.js
+++ language.js
@@ -65,11 +65,12 @@ module.exports = function() {
 
   return function getClientLang(req, res, next) {
     let language = req.headers["accept-language"] || DEFAULT_LANGUAGE;
-    language = "*";
-    language = language
-      .split(",")[0]
-      .split(";")[0]
-      .substring(0, 2); //["fr;q0.9", "en;0.8"] --> ["fr","q09"]
+    if (language != "*") {
+      language = language
+        .split(",")[0]
+        .split(";")[0]
+        .substring(0, 2); //["fr;q0.9", "en;0.8"] --> ["fr","q09"]
+    }
 
     let languages = Object.keys(TEXTS); // ["en","no"]
     if (!languages.indexOf(language)) {
