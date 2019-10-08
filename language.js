d4d85b2792b4a37becd8458d73beeba4ceea31b0
Sindre Bakken <sindreb98@hotmail.com>
2019-10-08 11:32:58 +0200

    fikser language og styler litt ved hjelp av CSS, takk til Toralf for hjelp med language fix

--- language.js
+++ language.js
@@ -4,6 +4,7 @@ module.exports = function() {
 
   const TEXTS = {
     en: {
+      START_STATEMENT: "Guess a number beweeen 1 and 100.",
       NOT_STARTED: "Game not started. Go to /start",
       WIN: "You guessed correctly ! Game over.",
       LOWER: "The number is bigger, try again!",
@@ -11,6 +12,7 @@ module.exports = function() {
       OVER: "The game is already over, too bad."
     },
     no: {
+      START_STATEMENT: "Gjett et tall mellom 1 og 100.",
       NOT_STARTED: "Ikke noe pågående spill. Gå til /start",
       WIN: "Du gjettet riktig! Game over.",
       LOWER: "Tallet er større, prøv på nytt!",
@@ -18,6 +20,7 @@ module.exports = function() {
       OVER: "Spillet er allerede over."
     },
     ro: {
+      START_STATEMENT: "Devinez la numărul 1 și 100.",
       NOT_STARTED: "Jocul nu a început. Mergi la /start",
       WIN: "Corect! Jocul s-a terminat",
       LOWER: "Numărul este mai mare, încearcă din nou!",
@@ -25,6 +28,7 @@ module.exports = function() {
       OVER: "Jocul s-a terminat deja."
     },
     fr: {
+      START_STATEMENT: "Devinez un nombre entre 1 et 100.",
       NOT_STARTED: "La partie n'a pas commence. Allez a /start",
       WIN: "Vous avez deviné correctement! Fin de la partie.",
       LOWER: "Le nombre est plus grand, reessaie!",
@@ -32,6 +36,7 @@ module.exports = function() {
       OVER: "La partie est deja terminee, dommage."
     },
     ar: {
+      START_STATEMENT: "تخمين عدد بين 1 و 100.",
       NOT_STARTED: "/start لم تبدا اللعبه, اذهب الى",
       WIN: ".لقد حزرت الرقم الصحيح. انتهت اللعبه",
       LOWER: "!الرقم اكبر, حاول مره ثانيه",
@@ -40,6 +45,7 @@ module.exports = function() {
     },
 
       de: {
+      START_STATEMENT: "Errate eine Zahl zwischen 1 und 100.",
       NOT_STARTED: "Das Spiel hat noch nicht begonnen. Gehe zu /start",
       WIN:"Richtig geraten! Das Spiel ist beendet.",
       LOWER:"Die Zahl ist grösser, versuche noch einmal!",
@@ -48,6 +54,7 @@ module.exports = function() {
     },
 
     bg: {
+      START_STATEMENT: "Познайте число между 1 и 100.",
       NOT_STARTED: "Играта не е започнала. Отиди до /start",
       WIN: "Ти позна правилния отговор! Край на играта.",
       LOWER: "Числото е по-голямо, опитай пак!",
@@ -69,7 +76,6 @@ module.exports = function() {
 return function getClientLang(req, res, next) {
   let language = req.headers["accept-language"] || DEFAULT_LANGUAGE;
   language = language.split(/[, -]/)[0].split(";")[0]; //["fr;q0.9", "en;0.8"] --> ["fr","q09"]
-  console.log(language);
   let languages = Object.keys(TEXTS); // ["en","no"]
   if(language === "nb" || "nb-no"){
     language = "no";
