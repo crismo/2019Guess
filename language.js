19f5945fa2213c7ec1ee417b92ded5c861bebe39
Sindre Bakken <sindreb98@hotmail.com>
2019-10-08 09:43:35 +0200

    fiksing av fieldset og noen  feil, samt fikset slik at norsk virket.

--- language.js
+++ language.js
@@ -65,11 +65,15 @@ module.exports = function() {
 
 };
 
+
 return function getClientLang(req, res, next) {
   let language = req.headers["accept-language"] || DEFAULT_LANGUAGE;
   language = language.split(/[, -]/)[0].split(";")[0]; //["fr;q0.9", "en;0.8"] --> ["fr","q09"]
-
+  console.log(language);
   let languages = Object.keys(TEXTS); // ["en","no"]
+  if(language === "nb" || "nb-no"){
+    language = "no";
+  }
   if (languages.indexOf(language) < 0) {
       language = DEFAULT_LANGUAGE;
   }
